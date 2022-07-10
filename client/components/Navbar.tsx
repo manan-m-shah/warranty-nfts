a87d8c7155f9ff66a6f085a8df803d18c189b6f3
manan-m-shah <shahmananvyara@gmail.com>
2022-07-10 10:51:44 +0530

    small changes

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='grid p-6 bg-color-base h-fit'>
-      <div className='card-pop-out-small p-1 justify-self-end '>
+      <div className='card-pop-out-small p-1 justify-self-end'>
         <button
-          className='p-3 bg-gray-200'>
+          className='p-3 rounded-lg bg-gray-200'>
           Connect
         </button>
       </div>
